7343948358d05d3e83db7efbb2fb0d5b94addae2
yzia2000 <e0381439@u.nus.edu>
2021-02-10 01:49:08 +0530

    moved to db.ts

--- core/src/config.ts
+++
@@ -1,7 +0,0 @@
-exports.dbConfig = {
-    host: 'db',
-    port: 5432,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB 
-};
